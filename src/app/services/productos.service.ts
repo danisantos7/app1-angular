a5be8755e5fc0489672dd344d8deda4f9446b443
Dani Santos <danielsantosolmo@gmail.com>
2021-11-10 18:57:12 +0100

    Fin ProductoService

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -9,16 +9,48 @@ export class ProductosService {
 
   cargando = true;
   productos: Producto[] = [];
+  productosFiltrado: Producto[] = [];
 
   constructor(private http: HttpClient) { 
     this.cargarProductos();
   }
 
   private cargarProductos() {
-    this.http.get('https://miweb-angular-default-rtdb.europe-west1.firebasedatabase.app/productos_idx.json')
-    .subscribe( (resp: any) => {
-      this.productos = resp
-      this.cargando = false;
+    return new Promise<void>((resolve, reject) => {
+      this.http.get('https://miweb-angular-default-rtdb.europe-west1.firebasedatabase.app/productos_idx.json')
+      .subscribe( (resp: any) => {
+        this.productos = resp
+        this.cargando = false;
+        resolve();
+      });
     });
+
+  }
+
+  getProducto (id: string) {
+    return this.http.get(`https://miweb-angular-default-rtdb.europe-west1.firebasedatabase.app/productos/${id}.json`);
+  }
+
+  buscarProducto (texto: string) {
+
+    if (this.productos.length === 0) {
+      this.cargarProductos().then(() =>{
+        this.filtrarProductos(texto);
+      });
+    } else {
+      this.filtrarProductos(texto);
+    }
+  }
+
+  private filtrarProductos(texto: string) {
+    console.log(this.productos);
+    this.productosFiltrado = [];
+    texto = texto.toLocaleLowerCase();
+    this.productos.forEach(prod =>{
+      const tituloLower = prod.titulo.toLocaleLowerCase();
+      if (prod.categoria.indexOf(texto) >= 0 || tituloLower.indexOf(texto) >=0) {
+        this.productosFiltrado.push(prod);
+      }
+    })
   }
 }
